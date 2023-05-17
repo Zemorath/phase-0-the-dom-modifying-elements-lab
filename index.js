b3376d1d1c0240bdc08b278986dd0facf2f19091
Zemorath <trentward10@gmail.com>
2023-05-16 21:58:38 -0500

    DOM Modifying Elements Complete

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+let main = document.getElementById("main");
+main.remove();
+
+const newHeader = document.createElement("h1");
+
+newHeader.setAttribute('id', 'victory');
+
+newHeader.textContent = "Trent is the champion";
